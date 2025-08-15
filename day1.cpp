b8bf5728fa4b2564758e9a28976d57c4d509f59b
Mansi-1225 <mansirs1225@gmail.com>
2025-08-15 23:48:58 +0530

    Update day1.cpp

--- day1.cpp
+++ day1.cpp
@@ -1,1 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
 
+   int n;
+   bool isPrime=true;
+   cout<<"enter a number to check if its prime or not : ";
+   cin>>n;
+   if(n<=0){
+    cout<<"invalid value entered";
+   }else{
+    for(int i=2; n>i; i++){
+        if(n%i==0){
+        isPrime=false;
+        break;
+    }
+   }
+    }
+    if(isPrime==true){
+        cout<<"entered number is Prime.";
+    } 
+    else{
+        cout<<"entered value is not prime";
+    }
+    return 0;
+}
